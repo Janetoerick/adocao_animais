5aa2323eaa3105e0d5ad2746853552179a72b40a
Janeto <janetoerick18@gmail.com>
2023-09-24 00:35:49 -0300

    add new animals

--- lib/components/lista_animais.dart
+++ lib/components/lista_animais.dart
@@ -13,66 +13,72 @@ class ListaAnimais extends StatelessWidget {
       padding: const EdgeInsets.all(8),
       itemCount: animais.length,
       itemBuilder: (ctx, index) {
-        return GestureDetector(
-          onTap: () {
-            // Navegação para a tela de detalhes do animal
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => AnimalDetalheScreen(animais[index]),
-              ),
-            );
-          },
-          child: Container(
-            height: 100,
-            color: Color(0x1f8a8a8a),
-            child: Row(
-              children: [
-                Stack(
-                  children: [
-                    Image.network(
-                      animais[index].img,
-                    ),
-                    (animais[index].novo)
-                        ? Positioned(
-                            child: Container(
-                              color: Theme.of(context).colorScheme.tertiary,
-                              child: Text("Novo"),
-                            ),
-                          )
-                        : Container()
-                  ],
+        return Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: GestureDetector(
+            onTap: () {
+              // Navegação para a tela de detalhes do animal
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => AnimalDetalheScreen(animais[index]),
                 ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
+              );
+            },
+            child: Container(
+              height: 100,
+              color: Color(0x1f8a8a8a),
+              child: Row(
+                children: [
+                  Stack(
                     children: [
-                      Text(
-                        animais[index].nome,
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.primary,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      Image.network(
+                        animais[index].img,
+                        height: 100,
+                        width: 150,
+                        fit: BoxFit.cover,
                       ),
-                      Text(
-                        '${animais[index].sexo} / ${animais[index].porte}',
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.secondary,
-                          fontSize: 18,
-                        ),
-                      ),
-                      Text(
-                        '${animais[index].idade}',
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.secondary,
-                          fontSize: 18,
-                        ),
-                      ),
+                      (animais[index].novo)
+                          ? Positioned(
+                              child: Container(
+                                color: Theme.of(context).colorScheme.tertiary,
+                                child: Text("Novo"),
+                              ),
+                            )
+                          : Container()
                     ],
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          animais[index].nome,
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '${animais[index].sexo} / ${animais[index].porte}',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.secondary,
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          '${animais[index].idade}',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.secondary,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         );
--- lib/screens/animal_screen.dart
+++ lib/screens/animal_screen.dart
@@ -29,6 +29,36 @@ class _AnimalPageState extends State<AnimalPage> {
       contato: '994412255',
       data_registro: DateTime.now(),
     ),
+    Animal(
+      id: 'l02',
+      novo: false,
+      tipo: 'Cachorro',
+      nome: 'Au o cão',
+      sexo: 'Femea',
+      porte: 'Medio',
+      idade: '1 ano e 2 meses',
+      img:
+          'https://t1.ea.ltmcdn.com/pt/posts/4/2/8/pedigree_de_cachorro_o_que_e_e_como_fazer_1824_600.jpg',
+      raca: 'Golden',
+      descricao: 'Peludo',
+      contato: '994455525',
+      data_registro: DateTime.now(),
+    ),
+    Animal(
+      id: 'l03',
+      novo: false,
+      tipo: 'Gato',
+      nome: 'Antonio Gebedaia',
+      sexo: 'Macho',
+      porte: 'Pequeno',
+      idade: '1 ano',
+      img:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3g1PlXY0Ua0aWAq6yTdOyl2kRyf-VlGdvQw&usqp=CAU',
+      raca: 'Viralata',
+      descricao: 'Toma remedio controlado',
+      contato: '996487115',
+      data_registro: DateTime.now(),
+    ),
   ];
 
   List<Animal> _animais_filter = [];
