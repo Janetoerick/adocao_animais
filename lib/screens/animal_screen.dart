a77038a355af0fea2a8ea83cfd95932b376cefa2
Janeto <janetoerick18@gmail.com>
2023-09-24 00:12:19 -0300

    att animal_screen

--- lib/screens/animal_screen.dart
+++ lib/screens/animal_screen.dart
@@ -2,6 +2,7 @@ import 'package:adocao_animais/components/filtro_animais.dart';
 import 'package:adocao_animais/components/lista_animais.dart';
 import 'package:flutter/material.dart';
 
+import '../components/filter_view.dart';
 import '../models/animal.dart';
 
 class AnimalPage extends StatefulWidget {
@@ -32,6 +33,7 @@ class _AnimalPageState extends State<AnimalPage> {
 
   List<Animal> _animais_filter = [];
   bool filter_on = false;
+  String filter = '';
 
   _openTaskFilterModal(BuildContext context) {
     showModalBottomSheet(
@@ -46,6 +48,7 @@ class _AnimalPageState extends State<AnimalPage> {
     _animais_filter = [];
     setState(() {
       if (tipo != '') {
+        filter = tipo;
         _animais_filter.addAll(_animais.where((e) => e.tipo == tipo));
       } else {
         filter_on = false;
@@ -55,53 +58,60 @@ class _AnimalPageState extends State<AnimalPage> {
     Navigator.of(context).pop();
   }
 
+  _remove_filter() {
+    setState(() {
+      filter = '';
+      filter_on = false;
+      _animais_filter = [];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Animal page"),
-          centerTitle: true,
-          backgroundColor: Theme.of(context).colorScheme.secondary,
-        ),
+        // appBar: AppBar(
+        //   title: Text("Animal page"),
+        //   centerTitle: true,
+        //   backgroundColor: Theme.of(context).colorScheme.secondary,
+        // ),
         body: Padding(
-          padding: EdgeInsets.all(10),
-          child: Column(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  IconButton(
-                    onPressed: () => _openTaskFilterModal(context),
-                    icon: Icon(Icons.filter_list),
-                  )
-                ],
-              ),
-              Expanded(
-                  child: SizedBox(
-                child: !filter_on
-                    ? ListaAnimais(_animais)
-                    : !_animais_filter.isEmpty
-                        ? ListaAnimais(_animais_filter)
-                        : Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Center(
-                              child: Container(
-                                child: Text(
-                                  "Não há esses animais registrados no momento...",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .primary),
-                                ),
-                              ),
-                            ),
-                          ),
-              ))
+              (filter_on) ? FilterView(filter, _remove_filter) : Container(),
+              IconButton(
+                onPressed: () => _openTaskFilterModal(context),
+                icon: Icon(Icons.filter_list),
+              )
             ],
           ),
-        ));
+          Expanded(
+              child: SizedBox(
+            child: !filter_on
+                ? ListaAnimais(_animais)
+                : !_animais_filter.isEmpty
+                    ? ListaAnimais(_animais_filter)
+                    : Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Center(
+                          child: Container(
+                            child: Text(
+                              "Não há esses animais registrados no momento...",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Theme.of(context).colorScheme.primary),
+                            ),
+                          ),
+                        ),
+                      ),
+          ))
+        ],
+      ),
+    ));
   }
 }
