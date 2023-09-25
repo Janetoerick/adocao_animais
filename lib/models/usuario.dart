a52f53c8fcc26a964f435dade5e5906fc69902e1
André Luis Gurgel de Faria Junior <50457552+andrejr95@users.noreply.github.com>
2023-09-25 16:30:14 -0300

    Usuario, login e cadastro
    Atualização de Usuario com métodos de login e cadastro e criação da base das telas de login e cadastro.
    Implementação do método de login nas telas faltando o método de cadastro.

--- lib/models/usuario.dart
+++ lib/models/usuario.dart
@@ -3,11 +3,90 @@ class Usuario {
   final String nome;
   final String email;
   final String telefone;
+  final String cpf;
+  final String login;
+  final String senha;
 
   const Usuario({
     required this.id,
     required this.nome,
     required this.email,
     required this.telefone,
+    required this.cpf,
+    required this.login,
+    required this.senha,
   });
+}
+
+class UsuarioInvalido extends Usuario {
+  UsuarioInvalido()
+      : super(
+          id: '',
+          nome: '',
+          email: '',
+          telefone: '',
+          cpf: '',
+          login: '',
+          senha: '',
+        );
+}
+
+// Classe de Gerenciamento de Usuários
+class GerenciadorUsuarios {
+  List<Usuario> _usuarios = [
+    Usuario(
+      id: '1',
+      nome: 'João',
+      email: 'joao@example.com',
+      telefone: '123456789',
+      cpf: '123.456.789-01',
+      login: 'joao123',
+      senha: 'senha123',
+    ),
+    Usuario(
+      id: '2',
+      nome: 'Maria',
+      email: 'maria@example.com',
+      telefone: '987654321',
+      cpf: '987.654.321-02',
+      login: 'maria456',
+      senha: 'senha456',
+    ),
+  ];
+
+  // Método para verificar credenciais de login
+  Usuario fazerLogin(String login, String senha) {
+    final usuario = _usuarios.firstWhere(
+      (u) => u.login == login && u.senha == senha,
+      orElse: () => UsuarioInvalido(),
+    );
+
+    return usuario;
+  }
+
+  // Método para verificar se um nome de usuário já está em uso
+  bool loginEmUso(String login) {
+    return _usuarios.any((u) => u.login == login);
+  }
+
+  // Método para adicionar um novo usuário
+  void cadastrarNovoUsuario({
+    required String nome,
+    required String email,
+    required String telefone,
+    required String cpf,
+    required String login,
+    required String senha,
+  }) {
+    final novoUsuario = Usuario(
+      id: DateTime.now().toString(),
+      nome: nome,
+      email: email,
+      telefone: telefone,
+      cpf: cpf,
+      login: login,
+      senha: senha,
+    );
+    _usuarios.add(novoUsuario);
+  }
 }
